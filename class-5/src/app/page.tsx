24c9ea9f38dd044f0b5cd3d7c46ce9f4b3273095
Bilal <bilalmk@gmail.com>
2024-10-05 22:50:48 +0500

    add class 5 code

---
+++ class-5/src/app/page.tsx
@@ -0,0 +1,19 @@
+"use client"
+
+export default function Home() {
+
+  function sayHello() {
+    console.log("Hello");
+  }
+  return (
+    <div className="flex justify-center items-center h-screen " >
+  <button onClick={sayHello}  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" >
+      <h1 className="text-lg" >
+      Click Me
+      </h1>
+    
+     </button>
+    </div>
+   
+  );
+}
