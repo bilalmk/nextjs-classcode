a3291f417d6569d8351061f8ae9ef3a28f28ad48
Bilal <bilalmk@gmail.com>
2024-10-26 11:49:05 +0500

    changes error

--- class-5/src/app/page.tsx
+++ class-5/src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 export default function Home() {
-
+  let test:number = 0
   function sayHello() {
     console.log("Hello");
   }
@@ -13,7 +13,7 @@ export default function Home() {
       </h1>
     
      </button>
-    </div>
+    <div>
    
   );
 }
